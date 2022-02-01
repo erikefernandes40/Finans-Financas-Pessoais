7832f6978b8a9f7417b07bf91790cb140f04b070
Erick Fernando <erikefernandes40@gmail.com>
2022-01-31 22:25:03 -0300

    Adição da janela modal e arquivo java script

---
+++ js.js
@@ -0,0 +1,13 @@
+const Modal = {
+    open(){
+        //abrir modal
+        //adicionar a classe active ao modal
+        document.querySelector('.modal-overlay').classList.add('active');
+    },
+    close(){
+        //fechar o modal
+        //remover a classe active do modal
+        document.querySelector('.modal-overlay').classList.remove('active');
+
+    }
+}
